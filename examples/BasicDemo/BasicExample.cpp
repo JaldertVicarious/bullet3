af4f22366fe9f1ff9b662cbc8b6ea61feedab51d
Jaldert Rombouts <jaldert@vicarious.com>
2022-09-06 18:03:49 +0000

    Hack in conveyor example

--- examples/BasicDemo/BasicExample.cpp
+++ examples/BasicDemo/BasicExample.cpp
@@ -16,14 +16,13 @@ subject to the following restrictions:
 #include "BasicExample.h"
 
 #include "btBulletDynamicsCommon.h"
-#define ARRAY_SIZE_Y 5
-#define ARRAY_SIZE_X 5
-#define ARRAY_SIZE_Z 5
 
 #include "LinearMath/btVector3.h"
 #include "LinearMath/btAlignedObjectArray.h"
 
 #include "../CommonInterfaces/CommonRigidBodyBase.h"
+#include <iostream>
+
 
 struct BasicExample : public CommonRigidBodyBase
 {
@@ -44,11 +43,51 @@ struct BasicExample : public CommonRigidBodyBase
 	}
 };
 
+
+
+static bool CustomMaterialCombinerCallback(btManifoldPoint& cp,	const btCollisionObjectWrapper* colObj0,int partId0,int index0,const btCollisionObjectWrapper* colObj1,int partId1,int index1)
+{
+
+	//std::cout << "in callback" << std::endl;
+	// No longer enabled 
+	// https://github.com/bulletphysics/bullet3/blob/daadfacfff365852ffc96f373c834216a25b11e5/src/BulletCollision/NarrowPhaseCollision/btManifoldPoint.h#L123
+	//cp.m_lateralFrictionInitialized = true;
+	
+	cp.m_contactPointFlags |= btContactPointFlags::BT_CONTACT_FLAG_LATERAL_FRICTION_INITIALIZED;
+	///choose a target velocity in the friction dir1 direction, for a conveyor belt effect
+	cp.m_lateralFrictionDir1.setValue(1.0,0.0,0.0);
+	cp.m_lateralFrictionDir1.normalize();
+
+	///optionally downscale the friction direction 2 for lower (anisotropic) friction (rather than a unit vector)
+	btScalar downscale = 1.f;
+	cp.m_lateralFrictionDir2 = downscale*cp.m_lateralFrictionDir1.cross(cp.m_normalWorldOnB);
+	
+	cp.m_contactMotion1 = 1.f;
+	//cp.m_contactCFM2 = 0.1;
+	//cp.m_combinedFriction = 10;
+	//cp.m_combinedRestitution = calculateCombinedRestitution(restitution0,restitution1);
+	return true;
+}
+
+
+
+
+
+
 void BasicExample::initPhysics()
 {
-	m_guiHelper->setUpAxis(1);
+	gContactAddedCallback = CustomMaterialCombinerCallback;
+
+	m_guiHelper->setUpAxis(2);
 
 	createEmptyDynamicsWorld();
+	//
+	m_dynamicsWorld->setGravity(btVector3(btScalar(0.), btScalar(0.), btScalar(-10)));
+	m_dynamicsWorld->getSolverInfo().m_solverMode |= SOLVER_DISABLE_VELOCITY_DEPENDENT_FRICTION_DIRECTION+SOLVER_USE_2_FRICTION_DIRECTIONS;
+	m_dynamicsWorld->getSolverInfo().m_solverMode |= SOLVER_ENABLE_FRICTION_DIRECTION_CACHING;
+
+
+
 	//m_dynamicsWorld->setGravity(btVector3(0,0,0));
 	m_guiHelper->createPhysicsDebugDrawer(m_dynamicsWorld);
 
@@ -56,59 +95,32 @@ void BasicExample::initPhysics()
 		m_dynamicsWorld->getDebugDrawer()->setDebugMode(btIDebugDraw::DBG_DrawWireframe + btIDebugDraw::DBG_DrawContactPoints);
 
 	///create a few basic rigid bodies
-	btBoxShape* groundShape = createBoxShape(btVector3(btScalar(50.), btScalar(50.), btScalar(50.)));
-
-	//groundShape->initializePolyhedralFeatures();
-	//btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0,1,0),50);
+	btBoxShape* groundShape = createBoxShape(btVector3(btScalar(1.), btScalar(0.5), btScalar(0.5)));
 
 	m_collisionShapes.push_back(groundShape);
 
 	btTransform groundTransform;
 	groundTransform.setIdentity();
-	groundTransform.setOrigin(btVector3(0, -50, 0));
+	groundTransform.setOrigin(btVector3(btScalar(0), btScalar(0), btScalar(0.5)));
 
 	{
 		btScalar mass(0.);
-		createRigidBody(mass, groundTransform, groundShape, btVector4(0, 0, 1, 1));
+		auto body = createRigidBody(mass, groundTransform, groundShape, btVector4(0, 0, 0, 1));
+		
+		//enable custom material callback
+		body->setCollisionFlags(body->getCollisionFlags()  | btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK);
 	}
 
 	{
-		//create a few dynamic rigidbodies
-		// Re-using the same collision is better for memory usage and performance
+		btBoxShape* boxShape = createBoxShape(btVector3(btScalar(0.1), btScalar(0.1), btScalar(0.1)));
+		m_collisionShapes.push_back(boxShape);
 
-		btBoxShape* colShape = createBoxShape(btVector3(.1, .1, .1));
-
-		//btCollisionShape* colShape = new btSphereShape(btScalar(1.));
-		m_collisionShapes.push_back(colShape);
-
-		/// Create Dynamic Objects
-		btTransform startTransform;
-		startTransform.setIdentity();
-
-		btScalar mass(1.f);
-
-		//rigidbody is dynamic if and only if mass is non zero, otherwise static
-		bool isDynamic = (mass != 0.f);
-
-		btVector3 localInertia(0, 0, 0);
-		if (isDynamic)
-			colShape->calculateLocalInertia(mass, localInertia);
-
-		for (int k = 0; k < ARRAY_SIZE_Y; k++)
-		{
-			for (int i = 0; i < ARRAY_SIZE_X; i++)
-			{
-				for (int j = 0; j < ARRAY_SIZE_Z; j++)
-				{
-					startTransform.setOrigin(btVector3(
-						btScalar(0.2 * i),
-						btScalar(2 + .2 * k),
-						btScalar(0.2 * j)));
-
-					createRigidBody(mass, startTransform, colShape);
-				}
-			}
-		}
+		
+		btTransform boxTransform;
+		boxTransform.setIdentity();
+		boxTransform.setOrigin(btVector3(btScalar(0), btScalar(0), btScalar(2.0)));
+		
+		auto body = createRigidBody(0.05, boxTransform, boxShape, btVector4(0, 0, 0, 1));
 	}
 
 	m_guiHelper->autogenerateGraphicsObjects(m_dynamicsWorld);
